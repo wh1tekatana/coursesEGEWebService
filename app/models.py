a3ab243fea37b88c757cdd7ffb403dde4ed6bdac
wh1tekatana <118110213+wh1tekatana@users.noreply.github.com>
2024-05-21 20:18:40 +0700

    Update models.py

--- app/models.py
+++ app/models.py
@@ -23,7 +23,7 @@ class Course(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     description = Column(String)
-    duration = Column(Integer)  # Duration in days
+    duration = Column(Integer)  # Длительность в днях
     start_date = Column(DateTime)
     students = relationship("Student", back_populates="course")
     teacher_id = Column(Integer, ForeignKey("teachers.id"))
